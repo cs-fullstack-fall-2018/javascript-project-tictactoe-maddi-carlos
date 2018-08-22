fb4e4dbb319b80af474a02a6857a6ba6e1d6955e
Lotus <cmadisonne@gmail.com>
2018-08-22 13:12:55 -0500

    switchign between x & o

--- t3.js
+++ t3.js
@@ -1,8 +1,8 @@
 var playerOne = "X";
 var playerTwo = "O";
 var score = {
-    'X':0 ,
-    'O':0
+    'X': 0,
+    'O': 0
 };
 var gridvalue = 0;
 
@@ -14,18 +14,34 @@ var gridvalue = 0;
 // Grab all the play spaces in the board
 var spaces = document.querySelectorAll("td");
 
-
-for (var x=0; x< spaces.length;x++){
+//console.log(spaces.length);
+for (var x = 0; x < spaces.length; x++) {
     spaces[x].addEventListener("click", function () {
-        this.innerText=playerOne});
-    if (this.innerText = playerOne){
-        this.innerText= playerTwo;
-    }
-    else if (this.innerText = playerTwo){
         this.innerText = playerOne;
-    }
+
+        console.log("X test: " + x);
+        switch (playerOne) {
+            case 'X':
+                playerOne = "O";
+                break;
+            case 'O':
+                playerOne = "X";
+                break;
+        }
+    });
+
+
+    console.log("PlayerOne is now: " + playerOne);
 }
 
+// function playerSwitch () {
+//     if (this.innerText = playerOne) {
+//         this.innerText = playerTwo;
+//     }
+//     else if (this.innerText = playerTwo) {
+//         this.innerText = playerOne;
+//     }
+//}
 
 
 // Clear all the play spaces in the board
