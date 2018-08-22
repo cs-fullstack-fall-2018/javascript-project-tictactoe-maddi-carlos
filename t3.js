a0cc4e0fc979b12255a188f767019a2a3eaf37ba
Lotus <cmadisonne@gmail.com>
2018-08-22 14:46:10 -0500

    Done

--- t3.js
+++ t3.js
@@ -14,37 +14,58 @@ var gridvalue = 0;
 // Grab all the play spaces in the board
 var spaces = document.querySelectorAll("td");
 
-//console.log(spaces.length);
-for (var x = 0; x < spaces.length; x++) {
+for (var x = 0; x < 9; x++) {
     spaces[x].addEventListener("click", function () {
-        this.innerText = playerOne;
+        if (this.innerText ==="") {
+            this.innerText = playerOne;
+            checkWinner();
 
-        console.log("X test: " + x);
-        switch (playerOne) {
-            case 'X':
-                playerOne = "O";
-                break;
-            case 'O':
-                playerOne = "X";
-                break;
+            switch (playerOne) {
+                case 'X':
+                    playerOne = "O";
+                    break;
+                case 'O':
+                    playerOne = "X";
+                    break;
+            }
         }
+        // var row1 = document.getElementById("row1");
+        // console.log(row1.innerText);
+        // if (row1.innerText = "X"){
+        //     alert("Player 1 Wins!")
+        //
+        // }
+
+
     });
 
-
-    console.log("PlayerOne is now: " + playerOne);
 }
 
-// function playerSwitch () {
-//     if (this.innerText = playerOne) {
-//         this.innerText = playerTwo;
-//     }
-//     else if (this.innerText = playerTwo) {
-//         this.innerText = playerOne;
-//     }
-//}
+function checkWinner() {
+    if (spaces[0].textContent === spaces[1].textContent && spaces[1].textContent === spaces[2].textContent && spaces[2].textContent === "X" ||
+        spaces[3].textContent === spaces[4].textContent && spaces[4].textContent === spaces[5].textContent && spaces[5].textContent === "X" ||
+        spaces[6].textContent === spaces[7].textContent && spaces[7].textContent === spaces[8].textContent && spaces[8].textContent === "X" ||
+        spaces[0].textContent === spaces[3].textContent && spaces[3].textContent === spaces[6].textContent && spaces[6].textContent === "X" ||
+        spaces[1].textContent === spaces[4].textContent && spaces[4].textContent === spaces[7].textContent && spaces[7].textContent === "X" ||
+        spaces[2].textContent === spaces[5].textContent && spaces[5].textContent === spaces[8].textContent && spaces[8].textContent === "X" ||
+        spaces[2].textContent === spaces[4].textContent && spaces[4].textContent === spaces[6].textContent && spaces[6].textContent === "X" ||
+        spaces[0].textContent === spaces[4].textContent && spaces[4].textContent === spaces[8].textContent && spaces[8].textContent === "X")
+    {
+        alert("X wins");
+    }
+        if (spaces[0].textContent === spaces[1].textContent && spaces[1].textContent === spaces[2].textContent && spaces[2].textContent === "O" ||
+            spaces[3].textContent === spaces[4].textContent && spaces[4].textContent === spaces[5].textContent && spaces[5].textContent === "O" ||
+            spaces[6].textContent === spaces[7].textContent && spaces[7].textContent === spaces[8].textContent && spaces[8].textContent === "O" ||
+            spaces[0].textContent === spaces[3].textContent && spaces[3].textContent === spaces[6].textContent && spaces[6].textContent === "O" ||
+            spaces[1].textContent === spaces[4].textContent && spaces[4].textContent === spaces[7].textContent && spaces[7].textContent === "O" ||
+            spaces[2].textContent === spaces[5].textContent && spaces[5].textContent === spaces[8].textContent && spaces[8].textContent === "O" ||
+            spaces[2].textContent === spaces[4].textContent && spaces[4].textContent === spaces[6].textContent && spaces[6].textContent === "O" ||
+            spaces[0].textContent === spaces[4].textContent && spaces[4].textContent === spaces[8].textContent && spaces[8].textContent === "O")
+        {
+            alert("O wins");
+    }}
 
 
-// Clear all the play spaces in the board
 
 // Setup click handlers for each play space on the board
 
---
+++ t32.js
@@ -0,0 +1,8 @@
+if (spaces[0].textContent === spaces[1].textContent && spaces[1].textContent === spaces[2].textContent && spaces[2].textContent === "X" ||)
+// spaces[3].textContent === spaces[4].textContent && spaces[4].textContent === spaces[5].textContent && spaces[5].textContent === "X" ||
+// spaces[6].textContent === spaces[7].textContent && spaces[7].textContent === spaces[8].textContent && spaces[8].textContent === "X" ||
+// spaces[0].textContent === spaces[3].textContent && spaces[3].textContent === spaces[6].textContent && spaces[6].textContent === "X" ||
+// spaces[1].textContent === spaces[4].textContent && spaces[4].textContent === spaces[7].textContent && spaces[7].textContent === "X" ||
+// spaces[2].textContent === spaces[5].textContent && spaces[5].textContent === spaces[8].textContent && spaces[8].textContent === "X" ||
+// spaces[0].textContent === spaces[4].textContent && spaces[4].textContent === spaces[7].textContent && spaces[7].textContent === "X" ||
+// spaces[2].textContent === spaces[4].textContent && spaces[4].textContent === spaces[6].textContent && spaces[6].textContent === "X" ||)
